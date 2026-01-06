e610db070d4d3a6aa93fe37d44082d38011f2b91
thiago-salaberry <thiagosalaberry08@gmail.com>
2026-01-06 13:59:50 -0300

    variables

---
+++ JS_INICIO/01-variables.js
@@ -0,0 +1,23 @@
+//var es un tipo de variable que se puede llamar y modiificar su vallr en cualquier parte del codigo
+
+var nombre = "Juan";
+console.log(nombre);    
+
+nombre = "Pedro";
+console.log(nombre);
+
+//let es un tipo de variable que se puede llamar y modificar su valor pero solo dentro del bloque donde fue declarada
+
+let edad = 25;
+console.log(edad);
+
+edad = 30;
+console.log(edad);
+
+//const es un tipo de variable que no se puede modificar su valor una vez declarada
+
+const pais = "Mexico";
+console.log(pais);
+
+//pais = "España"; //esto generaria un error
+//console.log(pais);

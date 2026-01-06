d4ad64e5bbf95db54076c3a6fe38e931e97cb852
thiago-salaberry <thiagosalaberry08@gmail.com>
2026-01-06 12:44:50 -0300

    inicio

---
+++ JS_INICIO/00-Console-log.js
@@ -0,0 +1,15 @@
+/*Console log sirve para poder escribir en la teminal o cosola del 
+navegador, ya sean datos, variables o cadenas de texto*/
+
+//cadena de texto
+console.log("hola mundo");
+
+var a = 10;
+
+//varible
+console.log(a);
+
+var b = 20;
+
+//operacion
+console.log(b + a);

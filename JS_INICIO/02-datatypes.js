edaab49c764378f17355260fc55fa5af8e20e540
thiago-salaberry <thiagosalaberry08@gmail.com>
2026-01-07 20:35:05 -0300

    Tipos de datos y ejercicios

---
+++ JS_INICIO/02-datatypes.js
@@ -0,0 +1,24 @@
+//String = cadena de texto
+let name = "Thiago"
+
+// Numeros
+let age = 17
+let height = 1.65
+
+// Booleanos
+let isStudent = true
+let isTeacher = false
+
+//No definidos
+let undefinedValue
+console.log(undefinedValue)
+
+//Valores nulos
+let nullValue = null
+
+//Symbol
+let mySymbol = Symbol("mysymbol")
+
+//Big int
+let myBigInt = BigInt(123456787654323456763223456787654323456789)
+let myBigInt2 = 23456787654323456763223456787654323456789n
---
+++ JS_INICIO/03-exercises.js
@@ -0,0 +1,59 @@
+// ===============================
+// Crear variables primitivas
+// ===============================
+let num = 10;                  // number
+let texto = "Hola";            // string
+let bool = true;               // boolean
+let indef                     // undefined
+let vacio = null;              // null
+let simbolo = Symbol("id");    // symbol
+let grande = 1234567890123n;   // bigint
+
+console.log("=== VALOR Y TIPO INICIAL ===");
+console.log(num, typeof num);
+console.log(texto, typeof texto);
+console.log(bool, typeof bool);
+console.log(indef, typeof indef);
+console.log(vacio, typeof vacio); 
+console.log(simbolo, typeof simbolo);
+console.log(grande, typeof grande);
+
+// =======================================
+// Modificar valores (MISMO TIPO)
+// =======================================
+num = 99;
+texto = "Chau";
+bool = false;
+indef = undefined;
+vacio = null;
+simbolo = Symbol("nuevo");
+grande = 9999999999999n;
+
+console.log("\n=== MISMO TIPO ===");
+console.log(num, typeof num);
+console.log(texto, typeof texto);
+console.log(bool, typeof bool);
+console.log(indef, typeof indef);
+console.log(vacio, typeof vacio);
+console.log(simbolo, typeof simbolo);
+console.log(grande, typeof grande);
+
+// =======================================
+// Modificar valores (DISTINTO TIPO)
+// =======================================
+num = "ahora soy texto";
+texto = 123;
+bool = null;
+indef = true;
+vacio = "ya no es null";
+simbolo = 42;        
+grande = 10;         
+
+console.log("\n=== DISTINTO TIPO ===");
+console.log(num, typeof num);
+console.log(texto, typeof texto);
+console.log(bool, typeof bool);
+console.log(indef, typeof indef);
+console.log(vacio, typeof vacio);
+console.log(simbolo, typeof simbolo);
+console.log(grande, typeof grande);
